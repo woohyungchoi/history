bdd2d9cba79242b05772f8f38b490e9a8e6436ec
최우형 <flrst24@naver.com>
2023-04-05 06:15:18 +0000

    hi3

--- 11654.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	char a;
-	scanf("%s",&a);
-	printf("%d",a);
-}
--- 15596.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char* argv[]) {
-	printf("Hello, goorm!\n");
-	
-	return 0;
-}
